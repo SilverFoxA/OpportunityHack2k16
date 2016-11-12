58c306b3aec9cdf243f6358c1dde6ce8cb597c1e
Shashank <pshashank88@gmail.com>
2016-11-12 19:31:36 +0530

    Ask question fragment

--- app/src/main/java/in/devmetric/opportunityhackcwdr/AskQuestionFragment.java
+++ app/src/main/java/in/devmetric/opportunityhackcwdr/AskQuestionFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -18,8 +19,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AskQuestionFragment extends Fragment {
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
